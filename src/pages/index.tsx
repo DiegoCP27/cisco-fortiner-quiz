d075d9df9a09f7778b8f989c46a7e8a6b26b0564
DiegoCP27 <dicornyt@gmail.com>
2023-04-25 19:12:16 -0500

    refactor: opciones multiples

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import { parseCookies } from 'nookies';
 import { destroyCookie } from 'nookies';
 
 interface QuestionOption {
-  answerByUser: null | string;
+  answerByUser: null | string[];
 }
 
 export default function Home({ questions }: any) {
@@ -44,32 +44,68 @@ export default function Home({ questions }: any) {
       }, 1000);
   };
   
-  const handleAnswerOption = (answer: any) => {
+  const handleAnswerOption = (answer: any, multipleSelection: boolean) => {
+    let updatedAnswerByUser: any = selectedOptions[currentQuestion]?.answerByUser;
+  
+    if (multipleSelection) {
+      if (!updatedAnswerByUser) {
+        updatedAnswerByUser = [answer];
+      } else if (Array.isArray(updatedAnswerByUser)) {
+        if (updatedAnswerByUser.includes(answer)) {
+          updatedAnswerByUser = updatedAnswerByUser.filter(
+            (item: any) => item !== answer
+          );
+        } else {
+          updatedAnswerByUser = [...updatedAnswerByUser, answer];
+        }
+      }
+    } else {
+      updatedAnswerByUser = answer;
+    }
+  
     const updatedOptions = [...selectedOptions];
-    updatedOptions[currentQuestion] = { answerByUser: answer };
+    updatedOptions[currentQuestion] = { answerByUser: updatedAnswerByUser };
     setSelectedOptions(updatedOptions);
   
     const updatedAnswerResults = [...answerResults];
-    const isCorrect = questions[currentQuestion].answerOptions.find((a: { isCorrect: any; }) => a.isCorrect).answer === answer;
+    const correctAnswers = questions[currentQuestion].answerOptions
+      .filter((a: { isCorrect: any }) => a.isCorrect)
+      .map((a: { answer: any }) => a.answer)
+      .sort();
+    const userAnswers = multipleSelection
+      ? updatedAnswerByUser.sort()
+      : [updatedAnswerByUser];
+    const isCorrect = JSON.stringify(userAnswers) === JSON.stringify(correctAnswers);
     updatedAnswerResults[currentQuestion] = isCorrect;
     setAnswerResults(updatedAnswerResults);
-    setSelectedAnswer(answer);
-  };
+    setSelectedAnswer(updatedAnswerByUser);
+  };  
   
   const handleSubmitButton = () => {
     let newScore = 0;
     for (let i = 0; i < questions.length; i++) {
-      questions[i].answerOptions.map(
-        (answer: any) =>
-          answer.isCorrect &&
-          answer.answer === selectedOptions[i]?.answerByUser &&
-          (newScore += 1)
-      );
+      const correctAnswers = questions[i].answerOptions
+        .filter((answer: any) => answer.isCorrect)
+        .map((answer: any) => answer.answer)
+        .sort();
+      const userAnswers: any = Array.isArray(selectedOptions[i]?.answerByUser)
+        ? selectedOptions[i]?.answerByUser
+        : [selectedOptions[i]?.answerByUser];
+  
+      const isCorrect =
+        JSON.stringify(userAnswers.sort()) ===
+        JSON.stringify(correctAnswers.sort());
+  
+      if (isCorrect) {
+        newScore += 1;
+      }
     }
     setScore(newScore);
     setPercentageScore((newScore / questions.length) * 100);
     setShowScore(true);
   };
+  
+
 
   return (
     <div className="todo">
@@ -107,25 +143,42 @@ export default function Home({ questions }: any) {
               )}
             </div>
             <div className="answer-options">
-            {questions[currentQuestion]?.answerOptions.map((answer: any, index: any) => (
-              <div
-                key={index}
-                className={`answer-option ${showAnswers ? (answer.isCorrect ? 'correct' : (selectedAnswer === answer.answer ? 'incorrect' : '')) : ''}`}
-                onClick={(e) => handleAnswerOption(answer.answer)}
-              >
-                <input
-                  type="radio"
-                  name={answer.answer}
-                  value={answer.answer}
-                  checked={
-                    answer.answer === selectedAnswer
-                  }
-                  onChange={(e) => handleAnswerOption(answer.answer)}
-                  className="radio"
-                />
-                <p className="answer-text">{answer.answer}</p>
-              </div>
-            ))}
+            {questions[currentQuestion]?.answerOptions.map((answer: any, index: any) => {
+              const multipleSelection = questions[currentQuestion].opcionMultiple;
+              const isSelected = multipleSelection
+                ? selectedOptions[currentQuestion]?.answerByUser?.includes(answer.answer)
+                : answer.answer === selectedAnswer;
+              const isCorrect = answer.isCorrect;
+
+              return (
+                <div
+                  key={index}
+                  className={`answer-option ${
+                    showAnswers
+                      ? isCorrect
+                        ? "correct"
+                        : isSelected
+                        ? "incorrect"
+                        : ""
+                      : ""
+                  }`}
+                >
+                  <label className="answer-label">
+                    <input
+                      type={multipleSelection ? "checkbox" : "radio"}
+                      name={`answer${currentQuestion}`}
+                      value={answer.answer}
+                      checked={isSelected}
+                      onChange={(e) =>
+                        handleAnswerOption(answer.answer, multipleSelection)
+                      }
+                      className="radio"
+                    />
+                    <p className="answer-text">{answer.answer}</p>
+                  </label>
+                </div>
+              );
+            })}
             </div>
             <div className="button-wrapper">
               <button
@@ -179,6 +232,7 @@ export async function getServerSideProps(context: any) {
     const questions = response.data.map((question: any) => {
       return {
         question: question.attributes.pregunta,
+        opcionMultiple: question.attributes.opcionMultiple,
         imagen: question.attributes.imagen?.data?.attributes?.url || null,
         answerOptions: question.attributes.respuestaOpcion.map((option: any) => {
           return {
