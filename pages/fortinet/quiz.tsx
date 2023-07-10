98e74d7ceb61f94aaec1e503a85c6f78d361e000
DiegoCP27 <dicornyt@gmail.com>
2023-07-10 01:06:54 -0500

    feat: agregar componentes a fortinet

---
+++ pages/fortinet/quiz.tsx
@@ -0,0 +1,78 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable @next/next/no-img-element */
+import { parseCookies } from "nookies";
+import { Quiz } from "@/components";
+
+type HomeProps = {
+  questions: any[];
+};
+
+export default function Home({ questions }: HomeProps) {
+  return <Quiz menuLink="fortinet" questions={questions} />;
+}
+
+export async function getServerSideProps(context: any) {
+  const cookies = parseCookies(context);
+  const token = cookies.token_fortinet;
+
+  // Si no hay token, redirige al usuario a la página de inicio de sesión
+  if (!token) {
+    return {
+      redirect: {
+        destination: "/fortinet",
+        permanent: false,
+      },
+    };
+  }
+
+  try {
+    // Si el token es válido, obtén las preguntas y devuélvelas al componente
+    const res = await fetch(
+      "https://strapi-production-d5a4.up.railway.app/api/preguntas?populate=*&sort[0]=id",
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    const response = await res.json();
+    let questions = response.data.map((question: any) => {
+      return {
+        question: question.attributes.pregunta,
+        opcionMultiple: question.attributes.opcionMultiple,
+        imagen: question.attributes.imagen?.data?.attributes?.url || null,
+        answerOptions: question.attributes.respuestaOpcion.map(
+          (option: any) => {
+            return {
+              isCorrect: option.isCorrect,
+              answer: option.respuesta,
+            };
+          }
+        ),
+      };
+    });
+
+    // Obtener los parámetros de consulta
+    const start = context.query.start;
+    const end = context.query.end;
+
+    // Filtrar las preguntas según el rango seleccionado por el usuario
+    if (start && end) {
+      questions = questions.slice(start - 1, end);
+    }
+
+    return {
+      props: {
+        questions,
+      },
+    };
+  } catch {
+    // Si el token no es válido, redirige al usuario a la página de inicio de sesión
+    return {
+      redirect: {
+        destination: "/fortinet",
+        permanent: false,
+      },
+    };
+  }
+}
